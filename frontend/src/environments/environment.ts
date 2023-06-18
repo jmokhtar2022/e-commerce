def05846ff68c836981c7a8ff5881d6cdcea719d
jmokhtar2022 <mohamedmokhtar.jaafar@esprit.tn>
2023-06-18 17:51:19 +0100

    up

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-export const apiUrl = '//backend-service:8080/api';
+export const apiUrl = '//localhost:8080/api';
 
 /*
  * For easier debugging in development mode, you can import the following file
